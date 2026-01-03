3bea172b96f0a42be857fcc8992d2e41328a4d62
Asifuzzaman Asif <asifuzzamanasif734@gmail.com>
2026-01-04 02:18:56 +0600

    Celery worker might solved

--- app/celery_app.py
+++ app/celery_app.py
@@ -10,7 +10,50 @@ celery = Celery(
     backend="redis://redis:6379/1",
 )
 
-celery.conf.timezone = "Asia/Dhaka"
+celery.conf.update( 
+    timezone= "UTC",
+    enable_utc= True,
+
+
+    #connection reliability
+    broker_connection_retry_on_startup= True,
+    broker_connection_retry= True,
+    broker_connection_max_retries= None,
+    borker_pool_limit= 10,
+
+
+    #Task configurations
+    task_acks_late=True,
+    task_reject_on_worker_lost= True,
+    worker_cancel_long_running_tasks_on_connection_loss= True,
+    worker_prefetch_multiplier= 4,
+    worker_max_tasks_per_child= 1000,
+
+
+    #Result backed
+    result_expire= 3600,
+    result_compression= 'gzip',
+
+    # Broker transport options
+    broker_transport_options={
+        "visibility_timeout": 600,  
+        "socket_keepalive": True,
+        "socket_connect_timeout": 30,
+        "socket_timeout": 30,
+        "health_check_interval": 30,
+        "max_connections": 50, 
+    },
+    
+    # Result backend transport options
+    result_backend_transport_options={
+        "socket_keepalive": True,
+        "socket_connect_timeout": 30,
+        "socket_timeout": 30,
+        "max_connections": 50,  
+    },
+
+
+)
 
 import app.schedule_task  # Ensure tasks are registered
 celery.conf.beat_schedule = {
